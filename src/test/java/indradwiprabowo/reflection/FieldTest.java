55e0181781af742b5a05082072f1c0d8d9e9c045
kaiharvest <indradwiprabowo27@gmail.com>
2025-12-01 14:55:28 +0700

    commit

---
+++ src/test/java/indradwiprabowo/reflection/FieldTest.java
@@ -0,0 +1,52 @@
+package indradwiprabowo.reflection;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
+public class FieldTest {
+
+    @Test
+    void testCreate() {
+        Class<Person> personClass = Person.class;
+
+        Field[] fields = personClass.getDeclaredFields();
+
+        for (Field field : fields) {
+            System.out.println(field.getName() + " : " + field.getType().getName());
+        }
+    }
+
+    @Test
+    void testGetField() throws NoSuchFieldException, IllegalAccessException {
+        Class<Person> personClass = Person.class;
+        Field firstName = personClass.getDeclaredField("firstName");
+        firstName.setAccessible(true);
+
+        Person person1 = new Person("Indra", "Dwi");
+
+        Object result = (String) firstName.get(person1);
+        System.out.println(result);
+
+        Person person2 = new Person("Joko", "Nugroho");
+        String result2 = (String) firstName.get(person2);
+        System.out.println(result2);
+    }
+
+    @Test
+    void testChanges() throws NoSuchFieldException, IllegalAccessException {
+        Class<Person> personClass = Person.class;
+        Field firstName = personClass.getDeclaredField("firstName");
+        firstName.setAccessible(true);
+
+        Person person = new Person("Budi", "Eko");
+
+        person.setFirstName(person, "Maling"); // person.setFirstName("Maling")
+        System.out.println(person.getFirstName());
+
+        Person person2 = new Person("Indra", "Dwi");
+        firstName.set(person2, "Tono"); // person2.setFirstName("Tono")
+        System.out.println(person2.getFirstName());
+    }
+
+}
---
+++ src/test/java/indradwiprabowo/reflection/MethodTest.java
@@ -0,0 +1,36 @@
+package indradwiprabowo.reflection;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+public class MethodTest {
+
+    @Test
+    void testGetMethod() {
+        Class<Person> personClass = Person.class;
+
+        Method[] methods = personClass.getDeclaredMethods();
+        for (Method method : methods) {
+            System.out.println(method.getName());
+            System.out.println(method.getReturnType());
+            System.out.println(Arrays.toString(method.getParameterTypes()));
+            System.out.println(Arrays.toString(method.getExceptionTypes()));
+            System.out.println("=====================");
+        }
+    }
+
+    @Test
+    void testGetMethodValue() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Class<Person> personClass = Person.class;
+        Method getFirstName = personClass.getDeclaredMethod("getFirstName");
+
+        Person person = new Person("Indra", "Dwi");
+
+        Object invoke = getFirstName.invoke(person);
+        System.out.println(invoke);
+    }
+
+}
