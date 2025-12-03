63fc139fc232625acb860ba10336af12f8138db8
kaiharvest <indradwiprabowo27@gmail.com>
2025-12-04 06:51:03 +0700

    Record

---
+++ src/test/java/indradwiprabowo/reflection/RecordTest.java
@@ -0,0 +1,47 @@
+package indradwiprabowo.reflection;
+
+import indradwiprabowo.reflection.data.Product;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.RecordComponent;
+import java.util.Arrays;
+
+public class RecordTest {
+
+    @Test
+    void testClassRecord() {
+        Class<Product> productClass = Product.class;
+
+        System.out.println(productClass.isRecord());
+        System.out.println(Arrays.toString(productClass.getDeclaredFields()));
+        System.out.println(Arrays.toString(productClass.getDeclaredMethods()));
+        System.out.println(Arrays.toString(productClass.getDeclaredConstructors()));
+        System.out.println(Arrays.toString(productClass.getRecordComponents()));
+    }
+
+    @Test
+    void testClassGetRecordValue() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Class<Product> productClass = Product.class;
+        Method id = productClass.getDeclaredMethod("id");
+
+        Product product = new Product("1", "Apple", 2000000L);
+        System.out.println(id.invoke(product));
+    }
+
+    @Test
+    void testGetRecordValueByComponent() throws InvocationTargetException, IllegalAccessException {
+        Class<Product> productClass = Product.class;
+        RecordComponent[] components = productClass.getRecordComponents();
+
+        Product product = new Product("1", "Iphone", 15000000L);
+
+        for (RecordComponent component : components) {
+            Method accessor = component.getAccessor();
+            System.out.println(accessor.getName());
+            System.out.println(accessor.invoke(product));
+        }
+    }
+
+}
